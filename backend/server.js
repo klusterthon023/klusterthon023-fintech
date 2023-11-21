c4d7b3df85066405b34935add24c14e5dc09618c
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-11-21 16:36:04 +0100

    Create backend structure

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello App!")
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to klusterthon023 Fintech Backend App" });
+});
+
+const POST = process.env.PORT || 5000;
+app.listen(POST, () => {
+  console.log(`Server is running on port ${POST}.`);
+});
