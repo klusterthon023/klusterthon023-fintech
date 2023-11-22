4ffa0624638cdb982c3d4b5599e521f5d116a9cf
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-23 02:27:50 +0300

    Centered the AboutUs image

--- frontend/src/pages/home/components/about-us/AboutUs-Image.tsx
+++ frontend/src/pages/home/components/about-us/AboutUs-Image.tsx
@@ -2,7 +2,7 @@
 
 export default function AboutUsImage() {
     return (
-        <div className="lg:mt-[-10%]">
+        <div className="lg:mt-[-10%] mx-auto md:mx-0">
             <img src="./src/assets/home/about-us-img.svg" alt="" />
         </div>
     )
