f551232b49d0943fc8c504ca039990f4d0b5164f
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 02:36:53 +0300

    Added maximum width for the paragraph so it won't strech on large screens

--- frontend/src/pages/register/components/signup-image.tsx
+++ frontend/src/pages/register/components/signup-image.tsx
@@ -9,7 +9,7 @@ export default function SignUpImage() {
             <img src={FinanceDiscussion} alt="" className=" xl:mx-auto" />
             <img src={dots} alt="" className=" absolute top-0 right-5 z-0 opacity-90" />
             <img src={dots} alt="" className=" absolute bottom-0 left-6 z-[-1] opacity-90" />
-            <div className="">
+            <div className="lg:max-w-[60ch] mx-auto">
                 <Typography className="relative z-20 text-center !text-4xl pb-2">
                 Efficient, Fast and Reliable</Typography>
                 <Typography className="!text-[#6F6F9A] z-0 text-center px-10 !text-base">
