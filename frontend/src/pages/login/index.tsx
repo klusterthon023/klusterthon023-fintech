3cc62b568eed173295766e94f92ffc165d8463d9
felixNT0 <tsowafelix0@gmail.com>
2023-11-23 16:04:23 +0100

    feature/FA-27/Setup API on FE

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,5 +1,21 @@
+// import { useMutation } from "react-query";
+// import { loginUser } from "./api-login";
+// import { ILoginPayload } from "./types";
+// import { Formik } from "formik";
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  // const { mutate, isLoading, isSuccess } = useMutation(loginUser);
+  // const handleSubmit = async (value: ILoginPayload) => {};
+  return (
+    <div>
+      {/* <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+      ></Formik> */}
+    </div>
+  );
 }
 
 export default LoginPage;
---
+++ frontend/src/react-query/index.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
+
+export const ApiAxiosInterceptor = axios.create({
+  baseURL: baseURL,
+});
