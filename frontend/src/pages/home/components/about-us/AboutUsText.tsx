2d2b9cbe00e26774c3b56656a2f73d4b0399d92a
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 19:26:28 +0300

    Updated the conent

--- frontend/src/pages/home/components/about-us/AboutUsText.tsx
+++ frontend/src/pages/home/components/about-us/AboutUsText.tsx
@@ -1,21 +1,35 @@
 import { Typography } from "../../../../design-system";
 import AboutUsStatics from "./AboutUsStatics";
 
-
 export default function AboutUsText() {
-    return (
-        <div className="flex flex-col gap-12">
-            <div className="grid gap-4">
-                <Typography color="secondary.300" className=" uppercase">ABOUT US</Typography>
-                <Typography variant="h3">We Are First of a Kind in Payment Solution Implementation</Typography>
-            </div>
-            <div className="flex flex-col gap-4">
-                <Typography color="gray.300">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore </Typography>
-                <Typography color="gray.300">Ut enim ad minima veniam, quis nostrum exercitationem
-                ullam corporis suscipit laboriosam, nisi ut al</Typography>
-            </div>
-            <AboutUsStatics />
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="grid gap-4">
+        <Typography color="secondary.300" className=" uppercase">
+          ABOUT US
+        </Typography>
+        <Typography variant="h3">
+          We Are First of a Kind in Payment Solution Implementation
+        </Typography>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Typography color="gray.300">
+          At{" "}
+          <Typography color="tertiary.300" className=" !inline-block">
+            InvoiceHub
+          </Typography>
+          , we believe in making business payments hassle-free. Our innovative
+          application empowers business owners to effortlessly create client
+          profiles, track payment transactions, and receive real-time alerts foe
+          unpaid invoices.{" "}
+        </Typography>
+        <Typography color="gray.300">
+          Say goodbye to payment woes and experience a streamlined payment
+          management system tailored just for you. InvoiceHub, a smarter way to
+          invoice!
+        </Typography>
+      </div>
+      <AboutUsStatics />
+    </div>
+  );
 }
