2b5c3e8a85b11bd751a39d281d0efbf7f9ffdb8d
Primesoft360 <elenduemmanuelelano@gmail.com>
2023-11-25 00:03:52 +0100

    fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -355,7 +355,7 @@ exports.resendToken = catchAsync(async (req, res, next) => {
 exports.resetPassword = catchAsync(async (req, res, next) => {
   // Get user based on the token
   const cookieHeader = req.headers.cookie;
-  console.log(cookieHeader);
+  console.log(cookieHeader, req.headers);
   let token;
   if (req.cookies && req.cookies.resetToken) {
     token = req.cookies.resetToken;
