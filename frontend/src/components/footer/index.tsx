4666da4bf626e2ce89637731d79ed4bb2ebd82dd
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 19:24:15 +0300

    Excluded the divider in the footer from being inside the container

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -4,15 +4,17 @@ import SupportBrands from "./SupportBrands";
 
 function Footer() {
   return (
-    <div className="px-16 max-md:px-8 pt-12 pb-9">
+    <div className="px-4 pt-12 pb-9">
       <SupportBrands />
       <Divider />
-      <FooterContext />
-      <Divider className="my-9" />
-      <div className="text-center">
-        <Typography color="gray.100" variant="body4">
-          &copy; {new Date().getFullYear()} All right reserved.
-        </Typography>
+      <div className=" container mx-auto">
+        <FooterContext />
+        <Divider className="my-9" />
+        <div className="text-center">
+          <Typography color="gray.100" variant="body4">
+            &copy; {new Date().getFullYear()} All right reserved.
+          </Typography>
+        </div>
       </div>
     </div>
   );
