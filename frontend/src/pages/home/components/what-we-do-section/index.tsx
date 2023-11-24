3266fa5c44d64322e8b883cab28f9d34e8e95d27
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 19:34:35 +0300

    Added padding

--- frontend/src/pages/home/components/what-we-do-section/index.tsx
+++ frontend/src/pages/home/components/what-we-do-section/index.tsx
@@ -1,12 +1,11 @@
 import WhatWeDoCard from "./WhatWeDoCard";
 import WhatWeDoTitle from "./WhatWeDoTitle";
 
-
 export default function WhatWeDoSection() {
-    return (
-        <section className="container mx-auto py-12 grid gap-12">
-        <WhatWeDoTitle />
-        <WhatWeDoCard />
-        </section>
-    )
+  return (
+    <section className="container mx-auto px-4 py-12 grid gap-12">
+      <WhatWeDoTitle />
+      <WhatWeDoCard />
+    </section>
+  );
 }
