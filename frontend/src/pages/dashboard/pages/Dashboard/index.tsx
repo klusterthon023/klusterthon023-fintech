c9a3465aa0d0035e6a0243c8f34f41b5e6a0aa22
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 21:30:05 +0300

    Added the dashboard's components

--- frontend/src/pages/dashboard/pages/Dashboard/index.tsx
+++ frontend/src/pages/dashboard/pages/Dashboard/index.tsx
@@ -1,9 +1,21 @@
-import { Typography } from "../../../../design-system";
+import QuickActions from "./components/quick-actions";
+import RecentNotifications from "./components/recent-notifications";
+import RecentTransactions from "./components/recent-transaction";
+import RevenueReports from "./components/revenue-report";
+import TopStatics from "./components/top-statics";
 
 function DashBoard() {
   return (
-    <div className="flex-1 p-10">
-      <Typography>This where the content will be</Typography>
+    <div className="flex-1 flex flex-col gap-5 p-10 bg-[#F0F0F4] h-full">
+      <TopStatics />
+      <RecentTransactions />
+      <div className="flex flex-col xl:grid xl:grid-cols-2 gap-5 h-full">
+        <div className="flex flex-col gap-5">
+          <QuickActions />
+          <RevenueReports />
+        </div>
+        <RecentNotifications />
+      </div>
     </div>
   );
 }
