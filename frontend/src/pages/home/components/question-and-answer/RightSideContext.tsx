8d41c475c136861b93c96d2918ed34f72b3d3a1d
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 19:33:45 +0300

    Updated the content

--- frontend/src/pages/home/components/question-and-answer/RightSideContext.tsx
+++ frontend/src/pages/home/components/question-and-answer/RightSideContext.tsx
@@ -39,25 +39,25 @@ export default RightSideContext;
 const LIST_FAQS = [
   {
     id: 1,
-    question: "Lorem ipsum dolor sit met",
+    question: "How do I get started using InvoiceHub?",
     answers:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "Simply sign up, create your account and begin managing payments seamlessly with our intuitive platform’s use-friendly interface.",
   },
   {
     id: 2,
-    question: "Lorem ipsum dolor sit met",
+    question: "Can I create and send invoices to my clients?",
     answers:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 3,
-    question: "Lorem ipsum dolor sit met",
+    question: "Is there a limit on number of clients  I can manage?",
     answers:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 4,
-    question: "Lorem ipsum dolor sit met",
+    question: "Can I set up recurring invoices for regular payments?",
     answers:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
