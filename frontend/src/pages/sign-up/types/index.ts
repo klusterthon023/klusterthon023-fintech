e21a15d6f3d6898a9d04d06166d2b07bf330ecb5
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 13:03:35 +0300

    Updated ISignUpPayload type to match the newest changes on the signup end point. Buessiness adress, description and name no longer needed to signup

--- frontend/src/pages/sign-up/types/index.ts
+++ frontend/src/pages/sign-up/types/index.ts
@@ -1,9 +1,6 @@
 export type ISignUpPayload = {
-  business_name: string;
   owner_name: string;
   email: string;
-  business_address: string;
-  business_description: string;
   password: string;
 };
 
