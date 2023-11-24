587caf527af7295f6ab71050f1025d13e3d13bd2
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 03:39:57 +0300

    Changed padding to match signup page

--- frontend/src/pages/login/components/LeftSideContext.tsx
+++ frontend/src/pages/login/components/LeftSideContext.tsx
@@ -3,22 +3,18 @@ import LoginForm from "./LoginForm";
 
 function LeftSideContext() {
   return (
-    <div className="p-16 max-md:p-8  max-sm:p-4 bg-color-gray w-full min-h-screen overflow-y-scroll">
-      <div className="bg-black p-3 h-12 w-[90px] flex justify-center items-center text-white rounded-lg mb-12">
-        LOGO
+    <div className="bg-color-gray py-12 px-4 lg:px-6 xl:px-12">
+      <div className="2xl:max-w-[1200px] text-2xl font-bold text-white bg-black py-3 px-[14px] w-[95px] h-[53px]
+            flex items-center justify-center rounded-[8px] mb-16">
+                LOGO
       </div>
-      <div className="px-12 max-md:p-3 text-center ">
-        <Typography className="!mb-3" variant="h5" fontWeight={600}>
-          Sign into your account
-        </Typography>
-        <div className="mb-6">
-          <Typography variant="body4" color="gray.300">
-            Manage your online payment transaction better.
-          </Typography>
+      <section className="grid gap-6 px-4 md:px-16 xl:max-w-[1000px] xl:mx-auto">
+        <div className="flex flex-col gap-3">
+          <Typography className="!text-[28px] !font-semibold text-center">Sign up for an account</Typography>
+          <Typography color="gray.300" className="text-center !text-sm">Manage your online payment transaction better.</Typography>
         </div>
-
         <LoginForm />
-      </div>
+      </section>
     </div>
   );
 }
