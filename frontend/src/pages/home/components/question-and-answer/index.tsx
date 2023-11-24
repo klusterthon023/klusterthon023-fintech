7dc1fe314e1a983ac6991a9a89ab71fad98eedad
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 19:32:34 +0300

    Added a container

--- frontend/src/pages/home/components/question-and-answer/index.tsx
+++ frontend/src/pages/home/components/question-and-answer/index.tsx
@@ -3,13 +3,14 @@ import RightSideContext from "./RightSideContext";
 
 function QuestionAndAnswer() {
   return (
-    <section
-      id="FAQ"
-      style={gradientStyle}
-      className="bg-faqs bg-opacity-100 container p-16 max-lg:p-8 w-full flex justify-between max-lg:justify-center max-lg:flex max-lg:flex-col max-lg:gap-10 items-center"
-    >
-      <LeftSideContext />
-      <RightSideContext />
+    <section id="FAQ" style={gradientStyle} className="">
+      <div
+        className="container mx-auto px-4 md:px-0 py-16 max-lg:py-8 w-full max-lg:justify-center
+       max-lg:gap-10 items-center flex flex-col md:flex-row justify-between "
+      >
+        <LeftSideContext />
+        <RightSideContext />
+      </div>
     </section>
   );
 }
