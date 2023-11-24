5bedfc259dfb1758b646ab883c72f9ac36b38a7f
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 21:28:59 +0300

    Added the app's logo

--- frontend/src/pages/dashboard/components/SideBar.tsx
+++ frontend/src/pages/dashboard/components/SideBar.tsx
@@ -5,6 +5,7 @@ import { Link, useLocation } from "react-router-dom";
 import { Typography } from "../../../design-system";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { RouteNames } from "../../../routers/interface";
+import logo from "../../../assets/invoice-hub-logo.svg";
 
 function SideBar() {
   const TAB_LIST = [
@@ -36,8 +37,11 @@ function SideBar() {
     pathname.split("/")[1] === itemPath.split("/")[1];
 
   return (
-    <div className="flex justify-between flex-col min-h-screen bg-white border-r border-color-gray px-5 py-6 min-w-[260px]">
-      <div>
+    <div className="min-h-screen bg-white border-r border-color-gray px-5 py-6 min-w-[260px]">
+      <div className="relative pb-12">
+        <img src={logo} alt="invoicehub" />
+      </div>
+      <div className="flex justify-between flex-col py-6">
         {TAB_LIST.map((item) => (
           <ListItem
             key={item.id}
@@ -63,25 +67,27 @@ interface IListItemProps {
 
 function ListItem({ label, path, icon, active }: IListItemProps) {
   return (
-    <Link to={path}>
-      <div
-        className={classNames(
-          "flex justify-start mt-2 !items-center gap-[14px] h-[52px] px-4 py-3 hover:bg-color-gray",
-          "rounded-lg text-gray-400",
-          {
-            ["!bg-color-primary !text-color-white"]: active,
-          }
-        )}
-      >
-        <FontAwesomeIcon icon={icon} />
-        <Typography
-          fontWeight={active ? 600 : 400}
-          variant={"body3"}
-          color={active ? "white" : "gray.400"}
+    <nav className="">
+      <Link to={path}>
+        <div
+          className={classNames(
+            "flex justify-start mt-2 !items-center gap-[14px] h-[52px] px-4 py-3 hover:bg-color-gray",
+            "rounded-lg text-gray-400",
+            {
+              ["!bg-color-primary !text-color-white"]: active,
+            }
+          )}
         >
-          {label}
-        </Typography>
-      </div>
-    </Link>
+          <FontAwesomeIcon icon={icon} />
+          <Typography
+            fontWeight={active ? 600 : 400}
+            variant={"body3"}
+            color={active ? "white" : "gray.400"}
+          >
+            {label}
+          </Typography>
+        </div>
+      </Link>
+    </nav>
   );
 }
