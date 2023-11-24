405918992d3ece20aa14f711e1657e3e0072706d
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-24 21:33:42 +0300

    Created a component that shows the user the revenue report

---
+++ frontend/src/pages/dashboard/pages/Dashboard/components/revenue-report.tsx
@@ -0,0 +1,9 @@
+import graphs from "../../../../../assets/dashaord/graphs.svg";
+
+export default function RevenueReports() {
+  return (
+    <section className="bg-white p-4 border border-gray-200 border-opacity-20 rounded-lg">
+      <img src={graphs} alt="revenue graphics" />
+    </section>
+  );
+}
