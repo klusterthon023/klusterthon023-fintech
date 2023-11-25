fc0daecfcca5e19f7c4f1911f656225652a586fc
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-26 02:03:22 +0300

    Intigrated backend

--- frontend/src/pages/dashboard/pages/Dashboard/components/top-statics.tsx
+++ frontend/src/pages/dashboard/pages/Dashboard/components/top-statics.tsx
@@ -1,44 +1,53 @@
+import { useMutation } from "react-query";
 import { Typography } from "../../../../../design-system";
+import { showStatics } from "../api-dashboard";
+import { useEffect, useState } from "react";
+import { dashboard } from "../types";
 
 export default function TopStatics() {
+  const [data, setData] = useState<dashboard | null>(null);
+  const { mutateAsync } = useMutation(showStatics);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await mutateAsync();
+        setData(result.data);
+        console.log(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [mutateAsync]);
+
   return (
     <section>
       <div className="flex flex-col md:flex-row gap-5 w-full">
         <div className="w-full bg-white border border-gray-200 border-opacity-20 rounded-lg p-4">
-          <Typography className="!text-sm !font-bold">Total Clients</Typography>
-          <Typography className="!font-bold"> - </Typography>
-          <Typography className="!flex !items-center gap-1">
-            {" "}
-            0%{" "}
-            <Typography color="gray.300" className="!text-xs">
-              VS last month
-            </Typography>
+          <Typography className="!text-sm !font-bold !pb-2">
+            Total Clients
+          </Typography>
+          <Typography className="!flex !items-center gap-1 !text-3xl !font-bold">
+            {data?.totalCustomers}
           </Typography>
         </div>
         <div className="w-full bg-white border border-gray-200 border-opacity-20 rounded-lg p-4">
-          <Typography className="!text-sm !font-bold">
+          <Typography className="!text-sm !font-bold !pb-2">
             Invoice Generated{" "}
           </Typography>
-          <Typography className="!font-bold"> - </Typography>
-          <Typography className="!flex !items-center gap-1">
-            {" "}
-            0%{" "}
-            <Typography color="gray.300" className="!text-xs">
-              VS last month
-            </Typography>
+          <Typography className="!flex !items-center gap-1 !text-3xl !font-bold">
+            {data?.invoicesGenerated}
           </Typography>
         </div>
         <div className="w-full bg-white border border-gray-200 border-opacity-20 rounded-lg p-4">
-          <Typography className="!text-sm !font-bold">
+          <Typography className="!text-sm !font-bold !pb-2">
             Revenue Generated
           </Typography>
-          <Typography className="!font-bold"> - </Typography>
-          <Typography className="!flex !items-center gap-1">
-            {" "}
-            0%{" "}
-            <Typography color="gray.300" className="!text-xs">
-              VS last month
-            </Typography>
+
+          <Typography className="!flex !items-center gap-1 !text-3xl !font-bold">
+            {data?.totalAmountGenerated}
           </Typography>
         </div>
       </div>
